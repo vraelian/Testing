f86b7dcb469a95e84ccdcab2d18bebcbf321ee7d
Devon Casey <devon_casey@outlook.com>
2026-04-22 19:42:05 -0700

    “Updated”

--- js/data/missions/phase_two.js
+++ js/data/missions/phase_two.js
@@ -8,7 +8,7 @@ export const PHASE_TWO_MISSIONS = {
         portraitId: "Venusian_Syndicate_4",
         isRepeatable: false,
         isAbandonable: false,
-        description: "Captain. I am Kiern, Research Director for the Venusian Syndicate. I have noted your recent logistical vectors. The Syndicate has highly lucrative work available for an aspiring operator who isn't paralyzed by Guild regulations.<br><br>But first, you must prove you are more than a debtor. <b>Amass a net worth of ⌬ 60,000</b> to demonstrate your merit in arbitrage, then <b>meet me at the Venusian cloud-cities</b>. I have forwarded a piece of intel to assist you.",
+        description: "Hello, Captain. I am Kiern, Research Director for the Venusian Syndicate. I have noted your recent logistical endeavors. The Syndicate has highly lucrative work available for an aspiring operator such as yourself, provided you aren't owned by the Merchant's Guild.<br><br>But first, you must prove you are more than a debtor. <b>Amass a net worth of ⌬ 60,000</b> to demonstrate your merit in arbitrage, then <b>meet me at the Venusian cloud-cities</b>. I have forwarded a piece of intel as an act of good faith.",
         triggers: [ { "type": "mission_completed", "missionId": "mission_17" } ],
         grantedIntel: [ { name: "Syndicate Tip", location: "loc_venus" } ],
         objectives: [
@@ -18,7 +18,7 @@ export const PHASE_TWO_MISSIONS = {
         completion: {
             locationId: "loc_venus",
             title: "Welcome to Venus",
-            text: "Captain. Your accounts reflect the requested liquidity. You have proven yourself capable of surviving the market; now let us see if you can manipulate it.",
+            text: "Captain. Your accounts reflect a respectable liquidity. You have proven yourself capable of surviving in this market; now let us see if you can manipulate it.",
             buttonText: "View Contracts"
         },
         rewards: []
@@ -31,7 +31,7 @@ export const PHASE_TWO_MISSIONS = {
         portraitId: "Venusian_Syndicate_4",
         isRepeatable: false,
         isAbandonable: false,
-        description: "The Guild relies on organic indentured labor to extract ice from Pluto. It is slow and agonizingly stable. Let's break their monopoly.<br><br><b>Purchase 15 Cybernetics and flood the Plutonian market with them.</b> The sudden availability of cheap, automated augmentation will shatter local labor values. Do not concern yourself with the immediate profit margin; the Syndicate will compensate you handsomely for the disruption.",
+        description: "The Guild relies on organic indentured labor to extract ice from Pluto. It is slow and agonizingly stable. I want to see you disrupt their monopoly.<br><br><b>Procure Cybernetics and flood the Plutonian market with them.</b> The sudden availability of cheap, automated augmentation will shatter local labor values. Do not concern yourself with the immediate profit margin; the Syndicate will compensate you handsomely for the disruption.",
         triggers: [ { "type": "mission_completed", "missionId": "mission_18" } ],
         objectives: [
             { "id": "have_cybernetics", "type": "HAVE_ITEM", "goodId": "cybernetics", "quantity": 15, "latch": true },
@@ -41,7 +41,7 @@ export const PHASE_TWO_MISSIONS = {
         completion: {
             locationId: "loc_pluto",
             title: "A Calculated Fracture",
-            text: "Excellent work. The labor camps on Pluto are already in localized revolt. You have helped us introduce a necessary chaos.",
+            text: "Excellent work. The labor camps on Pluto are already in localized revolt; the workforce paradigm is shifting rapidly. You have helped to introduce a necessary chaos.",
             buttonText: "Accept Payment"
         },
         rewards: [ { "type": "credits", "amount": 35000 } ]
